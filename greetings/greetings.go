ea7949fb69542f78bdb2a1fc6faaa93a18fc1f14
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2021-09-05 01:23:29 -0400

    first step done

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -5,6 +5,6 @@ import "fmt"
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprint("Hi, %v. Welcomes!", name)
+	message := fmt.Sprintf("Hi, %v. Welcomes!", name)
 	return message
 }
--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,11 @@
 package main
 
-import "fmt"
-
-import "rsc.io/quote"
+import ( 
+	"fmt"
+	"example.com/greetings"
+)
 
 func main() {
-	fmt.Println(quote.Go())
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
