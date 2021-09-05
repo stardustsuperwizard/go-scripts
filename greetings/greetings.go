e23e4f46f15246e152fe85b3aacb6476fdbfc5af
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2021-09-05 01:31:30 -0400

    added error handling

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// if no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcomes!", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,27 @@ package main
 
 import ( 
 	"fmt"
+	"log"
+
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Gladys")
+	// Set properties of the predefined Logger, including
+	// the log entry preix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	
+	// Request a greeting message
+	message, err := greetings.Hello("")
+	// if an error was returned, print it to the console and
+	// exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// if no error was returned, preint the returned message
+	// to the console.
 	fmt.Println(message)
 }
